a24eb4b919fcb32f4754bf9775e053a82276030f
Sofia Kristiansen <sofiakristiansen@Sofias-MBP.lan>
2017-02-27 15:44:41 +0100

    Added error handling for 403, 404 and 500 in form of handlebar-template pages. It has to be futher implemented to show the right error message at the right time.

--- app.js
+++ app.js
@@ -11,6 +11,7 @@
  let hbs = require('express-handlebars');
 
  let app = express();
+ let port = process.env.PORT || 8000;
 
  // View engine
  app.engine('.hbs', hbs({defaultLayout: 'main', extname: '.hbs'}));
@@ -22,6 +23,25 @@
  // Routes
  app.use('/', require('./routes/router.js'));
 
- app.listen(8000, function () {
-     console.log('Example app listening on port 8000!');
+ //Errors
+ app.use(function(req, res) {
+     res.status(404).render('error/404');
+ });
+
+ app.use(function(err, req, res, next) {
+    if (err.status !== 403) {
+        return next(err);
+    }
+    console.error(err.stack);
+    res.status(403).render('error/403');
+ });
+
+ app.use(function(err, req, res) {
+    console.error(err.stack);
+    res.status(500).render('error/500');
+ });
+
+ // Start listening
+ app.listen(port, function () {
+     console.log('Application listening on port ' + port + '.');
  });
